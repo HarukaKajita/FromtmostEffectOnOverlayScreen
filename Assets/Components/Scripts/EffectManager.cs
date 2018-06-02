57e29889d8dcc6637d35dd87d5c086dec25d9458
梶田悠 <kajitaj63b3@gmail.com>
2018-06-02 17:58:03 +0900

    initial commit

---
+++ Assets/Components/Scripts/EffectManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class EffectManager : MonoBehaviour {
+
+	[SerializeField] private GameObject _captureSetPrefab; //パーティクル撮影用カメラとパーティクルを子に持つEmptyObjectのプレハブ
+	[SerializeField] private GameObject _rawImagePrefab; //撮影して出来た映像を写すRawImageのプレハブ
+	
+	//RenderTextureの映像を写すRawImageをまとめる親オブジェクト
+	[SerializeField] private GameObject _rawImagesParentPrefab;//プレハブ
+	private Transform _rawImagesParent;//Instantiate後のインスタンスのトランスフォーム
+	
+	//最前面に来るCanvasをアタッチして下さい。
+	[SerializeField] private Transform _mostFrontCanvas;
+	
+	//プール
+	private readonly List<EffectSet> _effectPool = new List<EffectSet>();
+	[SerializeField] private uint _prePoolingNum = 3;//常時プールするエフェクトの数
+
+	//shrinkInterval秒毎に余分なエフェクトを1つずつ削除していきます。
+	public int ShrinkInterval = 10;
+
+	
+	private void Start()
+	{
+		if (_mostFrontCanvas == null)
+		{
+			_mostFrontCanvas = GameObject.Find("Canvas").transform;
+		}
+
+		SetRawImagesParent();
+		PoolDefaultEffects();
+
+		//クリックでエフェクトを再生するストリーム
+		Observable.EveryUpdate()
+			.Where(_ => Input.GetMouseButtonDown(0))
+			.Subscribe(_ => PlayEffect()).AddTo(this);
+
+		//定期的に余分なエフェクトを削除するストリーム
+		Observable
+			.Interval(TimeSpan.FromSeconds(ShrinkInterval))
+			.Where(_ => _effectPool.Count > 3)
+			.Select(_ => _effectPool[_effectPool.Count -1])
+			.Where(e => e.IsPlaying == false)
+			.Subscribe(ShrinkPool).AddTo(this);
+	}
+
+	private void SetRawImagesParent()
+	{
+		
+		_rawImagesParent = Instantiate(_rawImagesParentPrefab,_mostFrontCanvas).transform;
+		_rawImagesParent.transform.SetAsLastSibling();
+	}
+
+	private void PoolDefaultEffects()
+	{
+		//prePooligNum個エフェクトを予め作る(ここで作られたエフェクトは常にプールされ削除されない)
+		for(var i = 0; i < _prePoolingNum; i++){
+			var e = CreateInstance ();
+			_effectPool.Add (e);
+		}
+	}
+
+	private EffectSet CreateInstance(){
+
+		//エフェクトに関する物の参照をまとめるEffectSetクラスを生成
+		var effectSet = new EffectSet (_captureSetPrefab, _rawImagePrefab, transform, _rawImagesParent);
+
+		//他のカメラに他のパーティクルが写り込まないように位置を調整する
+		effectSet._effectCaptureSet.transform.localPosition = new Vector3 (_effectPool.Count * 10f,0f,0f);
+
+		return effectSet;
+	}
+
+
+	private void PlayEffect(){
+		var effect = SearchPlayableEffect ();
+		effect.PlayOn ();
+
+	}
+
+
+	private EffectSet SearchPlayableEffect(){
+		foreach(var e in _effectPool){
+			if(e.IsPlaying == false){
+				return e;
+			}
+		}
+
+		//再生できるエフェクトがない場合は新たにプールに追加
+		var newSakuraEffect = CreateInstance ();
+		//プールに追加
+		_effectPool.Add(newSakuraEffect);
+		return newSakuraEffect;
+	}
+
+	
+	 private void ShrinkPool(EffectSet e){
+		e.Delete ();
+		_effectPool.Remove (e);
+	}
+
+}
---
+++ Assets/Components/Scripts/EffectSet.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System;
+using Object = UnityEngine.Object;
+
+public class EffectSet{
+
+	public  GameObject _effectCaptureSet; //パーティクル撮影用カメラとパーティクルを子に持つEmptyObject
+	private readonly GameObject _effectRawImage; //撮影して出来たRenderTextureを写すRawImage
+
+	public bool IsPlaying;//パーティクル再生中であるかどうか
+
+
+	public EffectSet(GameObject captureSetPrefab, GameObject rawImagePrefab, Transform captureSetParent, Transform rawImageParent){
+		
+		_effectCaptureSet = Object.Instantiate (captureSetPrefab);
+		_effectRawImage = Object.Instantiate (rawImagePrefab);
+
+		//RenderTextureの設定
+		var rt = new RenderTexture (64,64,0);
+		var camera = _effectCaptureSet.transform.GetComponentInChildren<Camera> ();
+		camera.targetTexture = rt;
+		_effectRawImage.GetComponent<RawImage> ().texture = rt;
+
+		//ヒエラルキーを整える為親を設定
+		_effectCaptureSet.transform.SetParent (captureSetParent);
+		_effectRawImage.transform.SetParent (rawImageParent);
+
+		IsPlaying = false;
+	}
+
+	//エフェクト再生
+	public void PlayOn(){
+		IsPlaying = true;
+		//クリック位置にエフェクトが表示されるようにRawImageの位置を調整
+		//位置を変えたい場合はここをいじって下さい
+		Vector2 imagePos = Input.mousePosition;
+		imagePos.y -= _effectRawImage.GetComponent<RectTransform> ().rect.height / 2;
+		_effectRawImage.transform.position = imagePos;
+		_effectRawImage.GetComponent<RawImage> ().enabled = true;
+		var ps = _effectCaptureSet.transform.GetComponentInChildren<ParticleSystem> ();
+		ps.Play ();
+		Observable.Timer (TimeSpan.FromSeconds (ps.main.duration + ps.main.startLifetime.constant)).First ().Subscribe (_ => FinishPaling());
+	}
+
+	private void FinishPaling(){
+		IsPlaying = false;
+		_effectRawImage.GetComponent<RawImage> ().enabled = false;
+	}
+
+	public void Delete(){
+		Object.Destroy (_effectCaptureSet);
+		Object.Destroy (_effectRawImage);
+	}
+}
